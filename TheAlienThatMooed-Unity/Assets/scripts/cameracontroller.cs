90d06fec083b4d4ad8d6b05e507d130f274adfab
Wellwood97 <47885238+Wellwood97@users.noreply.github.com>
2019-05-23 09:46:35 +0100

    camera done, rotating power up.
    camera had to be deleted an redone now working. started work on the power-ups/collectables.

--- TheAlienThatMooed-Unity/Assets/scripts/cameracontroller.cs
+++ TheAlienThatMooed-Unity/Assets/scripts/cameracontroller.cs
@@ -1,13 +1,13 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class cameracontroller : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
 
-    public GameObject player;       //Public variable to store a reference to the player game object
+    public GameObject player;        //Public variable to store a reference to the player game object
 
 
-    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
 
     // Use this for initialization
     void Start()

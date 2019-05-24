d9dce5b5c96dd0892a19de10939973527c6cb4e4
RabbitGamer <jdakers1997@gmail.com>
2019-05-24 10:17:24 +0100

    Applied Music code

---
+++ TheAlienThatMooed-Unity/Assets/scripts/Music.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class Music : MonoBehaviour
+{
+
+    public AudioClip SoundClip1;
+
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+
+        audio.Play();
+        // Wait for the audio to have finished
+        yield return new WaitForSeconds(audio.clip.length);
+        // Assign the other clip and play its
+        audio.clip = SoundClip1;
+        audio.Play();
+    }
+}

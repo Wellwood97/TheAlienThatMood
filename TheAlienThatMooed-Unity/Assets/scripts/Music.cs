8ae37b5ffdea5e4e1036fdce1e7e32fa2059c9e1
RabbitGamer <jdakers1997@gmail.com>
2019-05-24 11:31:26 +0100

    BackgroundMusicCompleted

--- TheAlienThatMooed-Unity/Assets/scripts/Music.cs
+++ TheAlienThatMooed-Unity/Assets/scripts/Music.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 
 
-
 public class Music : MonoBehaviour
 {
-
+    // the length of the music audio file
+    public float loopLength;
+    // selecting the audio file
     public AudioClip SoundClip1;
 
 
@@ -15,12 +16,23 @@ public class Music : MonoBehaviour
     {
         AudioSource audio = GetComponent<AudioSource>();
 
-
+        // starts audio
         audio.Play();
-        // Wait for the audio to have finished
+        // wait for the audio to have finished
         yield return new WaitForSeconds(audio.clip.length);
-        // Assign the other clip and play its
+        // assign the other clip and play it
         audio.clip = SoundClip1;
+        audio.loop = true;
         audio.Play();
     }
+
+    //IEnumerator LoopCoroutine()
+    //{
+    //    while(true)
+    //    {   // wait for audio loop to finish
+    //        yield return new WaitForSeconds(loopLength);
+    //        // play audio
+    //        audio.PlayOneShot(SoundClip1);
+    //    }
+    //}
 }

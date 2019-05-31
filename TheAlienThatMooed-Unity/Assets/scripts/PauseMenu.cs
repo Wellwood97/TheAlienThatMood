fc34b974e29a0f773ed46df86ac604ce8bd5831d
StonedMorty <shadowek515@wp.pl>
2019-05-31 10:15:54 +0100

    I created a MenuScene and MainMenu scene to the game.

---
+++ TheAlienThatMooed-Unity/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+    public void Loadmain()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
+
